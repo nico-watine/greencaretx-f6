7b8a283025653477fc7fb81a5e1fd16cfc8b731a
Nico Watine <nico@studioncreations.com>
2021-08-06 15:26:49 -0500

    Clear file

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,15 +8,3 @@ og-image:
 body-id: index
 body-class: index
 ---
-<h1><b>hello</b></h1>
-<h1 style="font-weight: 700;">hello</h1>
-<h1 style="font-weight: 500;">hello</h1>
-<h1 style="font-weight: 400;">hello</h1>
-<h1>hello</h1>
-<h1 style="font-weight: 100;">hello</h1>
-<h1 style="font-family: 'Montserrat ExtraLight';">hello</h1>
-<p><strong>hello there</strong></p>
-<p style="font-weight: 700;">hello there</p>
-<p style="font-weight: 500;">hello there</p>
-<p style="font-weight: 400;">hello there</p>
-<p>hello</p>
