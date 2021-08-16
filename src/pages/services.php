9428714b3487a5394c3233f71e8df80789d58f4d
Nico Watine <nico@studioncreations.com>
2021-08-16 16:40:05 -0500

    Begin markup for service-option-card s

---
+++ src/pages/services.php
@@ -0,0 +1,30 @@
+---
+url: /services
+title: Services
+description:
+og-title:
+og-description:
+og-image:
+body-id: index
+body-class: index
+---
+<section class="services-section">
+<div class="row">
+	<h2 class="h2 font-weight-bold text-center mb-40">We offer 3 options to best suit your needs</h2>
+	<div class="medium-4 columns">
+		<div class="service-option-card">
+			&nbsp;
+		</div>
+	</div>
+	<div class="medium-4 columns">
+		<div class="service-option-card">
+			&nbsp;
+		</div>
+	</div>
+	<div class="medium-4 columns">
+		<div class="service-option-card">
+			&nbsp;
+		</div>
+	</div>
+</div>
+</section>
