cfbc1cf9bc59d7c81137dc6b3c5104e39f115596
Nico Watine <nico@studioncreations.com>
2021-08-16 16:42:25 -0500

    Set up h3s

--- src/pages/services.php
+++ src/pages/services.php
@@ -13,17 +13,17 @@ body-class: index
 	<h2 class="h2 font-weight-bold text-center mb-40">We offer 3 options to best suit your needs</h2>
 	<div class="medium-4 columns">
 		<div class="service-option-card">
-			&nbsp;
+			<h3 class="h3 font-weight-bold">Purchase new plants</h3>
 		</div>
 	</div>
 	<div class="medium-4 columns">
 		<div class="service-option-card">
-			&nbsp;
+			<h3 class="h3 font-weight-bold">Lease new plants</h3>
 		</div>
 	</div>
 	<div class="medium-4 columns">
 		<div class="service-option-card">
-			&nbsp;
+			<h3 class="h3 font-weight-bold">Service your existing plants</h3>
 		</div>
 	</div>
 </div>
