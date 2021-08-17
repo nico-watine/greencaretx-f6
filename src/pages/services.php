1be261c1effc7cb66a275485fe96f691db952811
Nico Watine <nico@studioncreations.com>
2021-08-17 18:29:02 -0500

    Markup progress

--- src/pages/services.php
+++ src/pages/services.php
@@ -9,7 +9,7 @@ body-id: index
 body-class: index
 ---
 <section class="services-section">
-	<div class="grid-container">
+	<div class="grid-container service-options-grid">
 		<h2 class="h2 font-weight-bold text-center mb-40 float-center">We offer 3 options to best suit your needs</h2>
 		<div class="grid-x grid-padding-x">
 			<div class="cell medium-4">
@@ -37,6 +37,21 @@ body-class: index
 						<p class="absolute-center width-100">If you happen to already have your own plants, we can take over their guaranteed maintenance.</p>
 					</div>
 					<a href="#" class="m-0 button large font-weight-bold mt-20 mb-0 border-rounded">Sign Up</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container purchase-lease-row">
+		<div class="row">
+			<h3 class="h3 font-weight-bold text-center mb-40 float-center" style="color: #fff;">What is the difference between a purchase and a lease?</h3>
+			<div class="medium-6 columns">
+				<div class="purchase-lease-explanation">
+					&nbsp;
+				</div>
+			</div>
+			<div class="medium-6 columns">
+				<div class="purchase-lease-explanation">
+					&nbsp;
 				</div>
 			</div>
 		</div>
