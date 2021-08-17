dc1f54935f723742b44567fd8d6bf49e804f7875
Nico Watine <nico@studioncreations.com>
2021-08-17 13:50:17 -0500

    Good progress on @small markup

--- src/pages/services.php
+++ src/pages/services.php
@@ -10,13 +10,13 @@ body-class: index
 ---
 <section class="services-section">
 	<div class="grid-container">
-		<h2 class="h2 font-weight-bold text-center mb-40">We offer 3 options to best suit your needs</h2>
+		<h2 class="h2 font-weight-bold text-center mb-40 float-center">We offer 3 options to best suit your needs</h2>
 		<div class="grid-x grid-padding-x">
 			<div class="cell medium-4">
 				<div class="service-option-card flex-container flex-dir-column">
 					<h3 class="h3 font-weight-bold">Purchase new plants</h3>
 					<div class="flex-child-auto position-relative">
-						<p class="lead absolute-center width-100">You purchase your new plants from us, along with their decorative containers, and we provide the weekly guaranteed maintenance.</p>
+						<p class="absolute-center width-100">You purchase your new plants from us, along with their decorative containers, and we provide the weekly guaranteed maintenance.</p>
 					</div>
 					<a href="#" class="m-0 button large font-weight-bold mt-20 mb-0 border-rounded">Sign Up</a>
 				</div>
@@ -25,7 +25,7 @@ body-class: index
 				<div class="service-option-card flex-container flex-dir-column">
 					<h3 class="h3 font-weight-bold">Lease new plants</h3>
 					<div class="flex-child-auto position-relative">
-						<p class="lead absolute-center width-100">We lease you the plants and their decorative containers, and we provide the weekly guaranteed maintenance.</p>
+						<p class="absolute-center width-100">We lease you the plants and their decorative containers, and we provide the weekly guaranteed maintenance.</p>
 					</div>
 					<a href="#" class="m-0 button large font-weight-bold mt-20 mb-0 border-rounded">Sign Up</a>
 				</div>
@@ -34,7 +34,7 @@ body-class: index
 				<div class="service-option-card flex-container flex-dir-column">
 					<h3 class="h3 font-weight-bold">Service your existing plants</h3>
 					<div class="flex-child-auto position-relative">
-						<p class="lead absolute-center width-100">If you happen to already have your own plants, we can take over their guaranteed maintenance.</p>
+						<p class="absolute-center width-100">If you happen to already have your own plants, we can take over their guaranteed maintenance.</p>
 					</div>
 					<a href="#" class="m-0 button large font-weight-bold mt-20 mb-0 border-rounded">Sign Up</a>
 				</div>
