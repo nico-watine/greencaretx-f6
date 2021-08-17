1655e3bc3049d24fa1beeca9cc49c9e55af10659
Nico Watine <nico@studioncreations.com>
2021-08-17 10:28:40 -0500

    Tentative markup for card

--- src/pages/services.php
+++ src/pages/services.php
@@ -37,23 +37,33 @@ body-class: index
 	<div class="grid-container">
 		<div class="grid-x grid-padding-x">
 			<div class="cell medium-4">
-				<div class="service-option-card">
+				<div class="service-option-card flex-container flex-dir-column">
 					<h3 class="h3 font-weight-bold">Purchase new plants</h3>
 					<p class="lead">You purchase your new plants from us, along with their decorative containers, and we provide the weekly guaranteed maintenance.</p>
 				</div>
 			</div>
 			<div class="cell medium-4">
-				<div class="service-option-card">
+				<div class="service-option-card flex-container flex-dir-column">
 					<h3 class="h3 font-weight-bold">Lease new plants</h3>
 					<p class="lead">We lease you the plants and their decorative containers, and we provide the weekly guaranteed maintenance.</p>
 				</div>
 			</div>
 			<div class="cell medium-4">
-				<div class="service-option-card">
-					<h3 class="h3 font-weight-bold">Service your existing plants</h3>
-					<p class="lead">If you happen to already have your own plants, we can take over their guaranteed maintenance.</p>
+				<div class="service-option-card flex-container flex-dir-column">
+					<div class="flex-child-auto">
+						<h3 class="h3 font-weight-bold">Service your existing plants</h3>
+					</div>
+					<div class="flex-child-auto">
+						<p class="lead">If you happen to already have your own plants, we can take over their guaranteed maintenance.</p>
+					</div>
+					<div class="flex-child-shrink align-self-middle width-100">
+						<a href="#" class="m-0 button expanded border-rounded">Sign Up</a>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </section>
+<br>
+<hr>
+<br>
