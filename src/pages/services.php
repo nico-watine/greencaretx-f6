a2f5d5ea3e24ddbba705e40cf72104ad2ca00002
Nico Watine <nico@studioncreations.com>
2021-08-17 10:55:17 -0500

    p.lead placed correctly vertically

--- src/pages/services.php
+++ src/pages/services.php
@@ -38,15 +38,15 @@ body-class: index
 		<div class="grid-x grid-padding-x">
 			<div class="cell medium-4">
 				<div class="service-option-card flex-container flex-dir-column">
-					<div class="flex-child-auto">
+					<!-- <div class="flex-child-auto"> -->
 						<h3 class="h3 font-weight-bold">Purchase new plants</h3>
+					<!-- </div> -->
+					<div class="flex-child-auto position-relative">
+						<p class="lead absolute-center width-100">You purchase your new plants from us, along with their decorative containers, and we provide the weekly guaranteed maintenance.</p>
 					</div>
-					<div class="flex-child-auto">
-						<p class="lead">You purchase your new plants from us, along with their decorative containers, and we provide the weekly guaranteed maintenance.</p>
-					</div>
-					<div class="flex-child-shrink align-self-middle width-100">
+					<!-- <div class="flex-child-shrink align-self-middle width-100"> -->
 						<a href="#" class="m-0 button expanded border-rounded">Sign Up</a>
-					</div>
+					<!-- </div> -->
 				</div>
 			</div>
 			<div class="cell medium-4">
@@ -64,15 +64,15 @@ body-class: index
 			</div>
 			<div class="cell medium-4">
 				<div class="service-option-card flex-container flex-dir-column">
-					<div class="flex-child-auto">
+					<!-- <div class="flex-child-auto"> -->
 						<h3 class="h3 font-weight-bold">Service your existing plants</h3>
+					<!-- </div> -->
+					<div class="flex-child-auto position-relative">
+						<p class="lead absolute-center width-100">If you happen to already have your own plants, we can take over their guaranteed maintenance.</p>
 					</div>
-					<div class="flex-child-auto">
-						<p class="lead">If you happen to already have your own plants, we can take over their guaranteed maintenance.</p>
-					</div>
-					<div class="flex-child-shrink align-self-middle width-100">
+					<!-- <div class="flex-child-shrink align-self-middle width-100"> -->
 						<a href="#" class="m-0 button expanded border-rounded">Sign Up</a>
-					</div>
+					<!-- </div> -->
 				</div>
 			</div>
 		</div>
