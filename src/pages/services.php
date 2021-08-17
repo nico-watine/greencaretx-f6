d1ac173035fd1d389329da5a70c7c31a8fc8a7a6
Nico Watine <nico@studioncreations.com>
2021-08-17 10:57:18 -0500

    Erase obsolete section

--- src/pages/services.php
+++ src/pages/services.php
@@ -9,32 +9,8 @@ body-id: index
 body-class: index
 ---
 <section class="services-section">
-<div class="row">
-	<h2 class="h2 font-weight-bold text-center mb-40">We offer 3 options to best suit your needs</h2>
-	<div class="medium-4 columns">
-		<div class="service-option-card">
-			<h3 class="h3 font-weight-bold">Purchase new plants</h3>
-			<p class="lead">You purchase your new plants from us, along with their decorative containers, and we provide the weekly guaranteed maintenance.</p>
-		</div>
-	</div>
-	<div class="medium-4 columns">
-		<div class="service-option-card">
-			<h3 class="h3 font-weight-bold">Lease new plants</h3>
-			<p class="lead">We lease you the plants and their decorative containers, and we provide the weekly guaranteed maintenance.</p>
-		</div>
-	</div>
-	<div class="medium-4 columns">
-		<div class="service-option-card">
-			<h3 class="h3 font-weight-bold">Service your existing plants</h3>
-			<p class="lead">If you happen to already have your own plants, we can take over their guaranteed maintenance.</p>
-		</div>
-	</div>
-</div>
-</section>
-<br>
-<hr>
-<section class="services-section">
 	<div class="grid-container">
+		<h2 class="h2 font-weight-bold text-center mb-40">We offer 3 options to best suit your needs</h2>
 		<div class="grid-x grid-padding-x">
 			<div class="cell medium-4">
 				<div class="service-option-card flex-container flex-dir-column">
@@ -66,6 +42,3 @@ body-class: index
 		</div>
 	</div>
 </section>
-<br>
-<hr>
-<br>
