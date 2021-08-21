8cfa2be932daaee925634426122bfa0771b90cb6
Nico Watine <nico@studioncreations.com>
2021-08-21 16:58:17 -0500

    Add bg ornament

--- src/pages/people-plants-slogan.php
+++ src/pages/people-plants-slogan.php
@@ -13,7 +13,7 @@ body-class: index
 		<div class="large-6 columns">
 			<img style="object-fit: cover; height: 500px; width: 100%;" src="/img/background-images/auditorium.jpg" alt="">
 		</div>
-		<div class="large-6 columns" style="height: 500px; position: relative;">
+		<div class="large-6 columns prose-column" style="height: 500px; position: relative;">
 			<div class="prose-wrap absolute-center">
 				<div class="prose float-center" style="width: 460px;">
 					<h3 class="separator-left">We’re not in the plant business, <br>serving people.</h3>
