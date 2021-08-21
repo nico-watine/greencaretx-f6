b26d90eb0a103eb4da03e1816c004516dc57bed7
Nico Watine <nico@studioncreations.com>
2021-08-21 09:38:48 -0500

    Place and size image

--- src/pages/people-plants-slogan.php
+++ src/pages/people-plants-slogan.php
@@ -9,5 +9,9 @@ body-id: index
 body-class: index
 ---
 <section class="people-plants-slogan-section">
-
+	<div class="row expanded collapse" style="height: 500px;">
+		<div class="large-6 columns">
+			<img style="object-fit: cover; height: 500px; width: 100%;" src="/img/background-images/auditorium.jpg" alt="">
+		</div>
+	</div>
 </section>
