3960d385fd4e811791afe90264c4a0093aa99f19
Nico Watine <nico@studioncreations.com>
2021-08-21 09:47:04 -0500

    Get prose-wrap aligned and placed correctly

--- src/pages/people-plants-slogan.php
+++ src/pages/people-plants-slogan.php
@@ -9,9 +9,16 @@ body-id: index
 body-class: index
 ---
 <section class="people-plants-slogan-section">
-	<div class="row expanded collapse" style="height: 500px;">
+	<div class="row expanded collapse">
 		<div class="large-6 columns">
 			<img style="object-fit: cover; height: 500px; width: 100%;" src="/img/background-images/auditorium.jpg" alt="">
+		</div>
+		<div class="large-6 columns" style="height: 500px; position: relative;">
+			<div class="prose-wrap absolute-center">
+				<div class="prose float-center" style="width: 460px;">
+					<h3>We’re not in the plant business, <br>serving people.</h3>
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
