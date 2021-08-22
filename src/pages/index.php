b7e7bfb5600d9c18fc8348edbf9ef0bcc3699458
Nico Watine <nico@studioncreations.com>
2021-08-22 18:43:14 -0500

    Combine sections onto index

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,3 +8,71 @@ og-image:
 body-id: index
 body-class: index
 ---
+<section class="people-plants-slogan-section">
+	<div class="row expanded collapse">
+		<div class="show-for-large large-6 columns media-column">
+			<img src="/img/background-images/auditorium.jpg" alt="">
+		</div>
+		<div class="large-6 columns prose-column">
+			<div class="prose-wrap">
+				<div class="prose">
+					<h3 class="h3 separator-left">We’re not in the plant business, <br>serving people.</h3>
+					<h3 class="h3 font-weight-bold font-italic">We are in the people business, <br>servicing plants.</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<br>
+<section class="services-section">
+	<div class="grid-container service-options-grid">
+		<h2 class="h2 section-heading font-weight-bold text-center mb-40 float-center">We offer 3 options to best suit your needs</h2>
+		<div class="grid-x grid-padding-x">
+			<div class="cell medium-4">
+				<div class="service-option-card flex-container flex-dir-column">
+					<h3 class="h3 font-weight-bold">Purchase new plants</h3>
+					<div class="flex-child-auto position-relative">
+						<p class="absolute-center width-100">You purchase your new plants from us, along with their decorative containers, and we provide the weekly guaranteed maintenance.</p>
+					</div>
+					<a href="#" class="m-0 button large font-weight-bold mt-20 mb-0 border-rounded">Get Quote</a>
+				</div>
+			</div>
+			<div class="cell medium-4">
+				<div class="service-option-card flex-container flex-dir-column">
+					<h3 class="h3 font-weight-bold">Lease new plants</h3>
+					<div class="flex-child-auto position-relative">
+						<p class="absolute-center width-100">We lease you the plants and their decorative containers, and we provide the weekly guaranteed maintenance.</p>
+					</div>
+					<a href="#" class="m-0 button large font-weight-bold mt-20 mb-0 border-rounded">Get Quote</a>
+				</div>
+			</div>
+			<div class="cell medium-4">
+				<div class="service-option-card flex-container flex-dir-column">
+					<h3 class="h3 font-weight-bold">Service your existing plants</h3>
+					<div class="flex-child-auto position-relative">
+						<p class="absolute-center width-100">If you happen to already have your own plants, we can take over their guaranteed maintenance.</p>
+					</div>
+					<a href="#" class="m-0 button large font-weight-bold mt-20 mb-0 border-rounded">Get Quote</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container purchase-lease-row">
+		<div class="row">
+			<h3 class="h3 section-heading font-weight-bold text-center mb-40 float-center">What is the difference between a purchase and a lease?</h3>
+			<div class="medium-6 columns">
+				<div class="purchase-lease-explanation" id="purchase">
+					<h4 class="h4 font-weight-bold">Purchase plants</h4>
+					<p>When purchasing your plants and containers, the larger up-front cost is offset by a lower monthly fee.</p>
+				</div>
+			</div>
+			<div class="medium-6 columns">
+				<div class="purchase-lease-explanation" id="lease">
+					<h4 class="h4 font-weight-bold">Lease plants</h4>
+					<p>By leasing your plants, the initial expense of a purchase is avoided. The monthly fee of a lease, however, can be up to 50% higher than when purchasing plants.</p>
+				</div>
+			</div>
+			<h3 class="h3 section-heading font-weight-bold text-center float-center">You receive the same great service with both options!</h3>
+		</div>
+	</div>
+</section>
