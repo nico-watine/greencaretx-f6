140b3ad431b907f4e5b0f81f46bbf12290a436ae
Nico Watine <nico@studioncreations.com>
2021-08-22 10:56:32 -0500

    Getting @small set up

--- src/pages/people-plants-slogan.php
+++ src/pages/people-plants-slogan.php
@@ -10,11 +10,11 @@ body-class: index
 ---
 <section class="people-plants-slogan-section">
 	<div class="row expanded collapse">
-		<div class="large-6 columns media-column">
+		<div class="show-for-large large-6 columns media-column">
 			<img src="/img/background-images/auditorium.jpg" alt="">
 		</div>
-		<div class="large-6 columns prose-column" style="height: 500px; position: relative;">
-			<div class="prose-wrap vertical-center ml-32">
+		<div class="large-6 columns prose-column">
+			<div class="prose-wrap">
 				<h3 class="h3 separator-left">We’re not in the plant business, <br>serving people.</h3>
 				<h3 class="h3 font-weight-bold font-italic">We are in the people business, <br>servicing plants.</h3>
 			</div>
