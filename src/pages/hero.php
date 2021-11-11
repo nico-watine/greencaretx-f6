5346e69f0335f95ffa3d257ad34b1c97ef0e9e71
Nico Watine <nico@studioncreations.com>
2021-11-11 10:24:15 -0600

    Add svg logo and insert via img tag

--- src/pages/hero.php
+++ src/pages/hero.php
@@ -14,10 +14,10 @@ body-class: index
 		<div class="top-bar">
 			<div class="top-bar-left">
 				<ul class="menu">
-					<li class="menu-text"><img src="http://placehold.it/75x30" alt="logo"></li>
-					<li><a href="#">One</a></li>
-					<li><a href="#">Two</a></li>
-					<li><a href="#">Three</a></li>
+					<li class="menu-text"><img src="/img/logo/greencaretx-logo.svg" alt="logo" style="width: 100px;"></li>
+					<!-- <li><a href="#">One</a></li> -->
+					<!-- <li><a href="#">Two</a></li> -->
+					<!-- <li><a href="#">Three</a></li> -->
 				</ul>
 			</div>
 		</div>
