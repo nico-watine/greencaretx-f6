9905e03f2fae124316c7ad8a82202bedeee51b48
Nico Watine <nico@studioncreations.com>
2021-12-07 17:30:55 -0600

    Move hero page contents to index

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,6 +8,39 @@ og-image:
 body-id: index
 body-class: index
 ---
+<div class="hero-full-screen">
+
+	<div class="top-content-section">
+		<div class="top-bar">
+			<div class="top-bar-left">
+				<ul class="menu">
+					<li class="menu-text"><img src="/img/logo/greencaretx-logo.svg" alt="logo" style="width: 100px;"></li>
+					<li><a href="#">One</a></li>
+					<li><a href="#">Two</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="prose-wrap">
+		<h1 class="headline">Servicing Plants <br><span>Serving People</span></h1>
+		<h2 class="mission-statement float-center">As a family-owned business, providing top-tier plant service to ensure your complete satisfaction is our highest&nbsp;priority.</h2>
+	</div>
+
+	<div class="bottom-content-section text-center" data-magellan data-threshold="0">
+		<a href="#main-content-section" class="cta button hollow border-rounded" style="font-size: 16px;">Schedule your free consultation</a>
+		<br>
+		<!-- circle with downwards arrow -->
+		<!-- <a href="#main-content-section"><svg viewBox="0 0 52 52" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="Heros" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="1200-(16:9)" transform="translate(-574.000000, -596.000000)" stroke="#FFFFFF" stroke-width="2"><g id="Arrow-down" transform="translate(600.000000, 622.000000) rotate(-90.000000) translate(-600.000000, -622.000000) translate(575.000000, 597.000000)"><path d="M29.9233479,36.7014989 L19.1259765,25.7251304 C19.0462991,25.6397453 19,25.5280878 19,25.4131463 C19,25.2971101 19.0462991,25.1865473 19.1259765,25.1044462 L29.9265781,14.1280777 C30.0956235,13.9573074 30.3691109,13.9573074 30.5327727,14.1280777 C30.7007414,14.298848 30.7007414,14.576897 30.5327727,14.7476673 L20.039037,25.4131463 L30.5327727,36.0819094 C30.7007414,36.2526796 30.7007414,36.5307287 30.5327727,36.7014989 C30.4487884,36.7868841 30.3389627,36.8295766 30.2302138,36.8295766 C30.1203881,36.8295766 30.0116391,36.7868841 29.9233479,36.7014989 Z" id="Fill-3"></path><path d="M25,50 C38.8071187,50 50,38.8071187 50,25 C50,19.2875591 48.0840719,14.0226073 44.8597553,9.81268399 C40.2908144,3.84711279 33.0946779,0 25,0 C11.1928813,0 0,11.1928813 0,25 C0,38.8071187 11.1928813,50 25,50 Z" id="Oval-4"></path></g></g></g></svg></a> -->
+	</div>
+
+</div>
+
+<div id="main-content-section" data-magellan-target="main-content-section">
+	<!-- your content goes here -->
+	<h1>lipsum</h1>
+</div>
+
 <section class="people-plants-slogan-section">
 	<div class="row expanded collapse">
 		<div class="show-for-large large-6 columns media-column">
