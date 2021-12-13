a44e1455491573151f8c34a8f066c2f509347112
Nico Watine <nico@studioncreations.com>
2021-12-13 11:34:50 -0600

    @large done

--- src/pages/scheduling.php
+++ src/pages/scheduling.php
@@ -8,3 +8,17 @@ og-image:
 body-id: index
 body-class: index
 ---
+<section class="scheduling-banner py-40">
+	<div class="row">
+		<div class="large-7 columns">
+			<div class="prose-wrap">
+				<p class="h4 float-right">To schedule an appointment and next-day quote, <br>please call or email our office administrator, <br>Lissette Martinez.</p>
+			</div>
+		</div>
+		<div class="large-5 columns">
+			<div class="prose-wrap">
+				<p class="h3 font-weight-bold text-center">713-864-5300 <br>lissette@greencaretx.com</p>
+			</div>
+		</div>
+	</div>
+</section>
