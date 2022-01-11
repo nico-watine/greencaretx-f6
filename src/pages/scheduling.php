1f07cde1a9867cc159aa75ca6b6e8921e514f2fb
Nico Watine <nico@studioncreations.com>
2022-01-11 09:26:25 -0600

    Set up scheduling structure correctly

--- src/pages/scheduling.php
+++ src/pages/scheduling.php
@@ -11,6 +11,21 @@ body-class: index
 <section class="scheduling-banner py-40">
 	<div class="row">
 		<div class="large-7 columns">
+			<div class="prose-wrap float-center large-float-right">
+				<p class="h4 scheduling-prose">To schedule an appointment and next-day quote, please call or email our office administrator, Lissette&nbsp;Martinez.</p>
+			</div>
+		</div>
+		<div class="large-5 columns">
+			<div class="prose-wrap float-center">
+				<p class="h3 font-weight-bold text-center">713-864-5300 <br>lissette@greencaretx.com</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- <section class="scheduling-banner py-40">
+	<div class="row">
+		<div class="large-7 columns">
 			<div class="prose-wrap float-right" style="max-width: 530px;">
 				<p class="h4">To schedule an appointment and next-day quote, please call or email our office administrator, Lissette Martinez.</p>
 			</div>
@@ -21,4 +36,4 @@ body-class: index
 			</div>
 		</div>
 	</div>
-</section>
+</section> -->
