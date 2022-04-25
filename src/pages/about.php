ed5fd2d18bd70ef715b2681bdbd2ddfd846137ba
Nico Watine <nico@studioncreations.com>
2022-04-25 11:20:03 -0500

    Add p and text-shadow

--- src/pages/about.php
+++ src/pages/about.php
@@ -11,5 +11,7 @@ body-class: index
 <section class="about-us-section">
 	<div class="content text-center">
 		<h1 class="font-weight-bold">About Us</h1>
+		<p class="h4">Founded in 2002, Greencare Interior Plants, LLC, is a family-owned and operated business. We take pride in our reputation for reliable service, original design and great care of our customers and employees.</p>
+		<p class="h4">Working with both Commercial and Residential accounts throughout the Greater Houston area, we have the maintenance program and greenery selection to exceed the professional plant care service you expect.</p>
 	</div>
 </section>
