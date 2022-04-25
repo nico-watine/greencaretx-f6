78f7d63577b08416a3bd1de7544ceb88b7cc6b22
Nico Watine <nico@studioncreations.com>
2022-04-25 11:52:14 -0500

    Replace line-break with contact banner

--- src/pages/index.php
+++ src/pages/index.php
@@ -116,7 +116,20 @@ body-class: index
 		</div>
 	</div>
 </section>
-<br>
+<section class="scheduling-banner py-40">
+	<div class="row">
+		<div class="large-7 columns">
+			<div class="prose-wrap float-center large-float-right">
+				<p class="h4 scheduling-prose">To schedule an appointment and next-day quote, please call or email our office administrator, Lissette&nbsp;Martinez.</p>
+			</div>
+		</div>
+		<div class="large-5 columns">
+			<div class="float-center">
+				<p class="h3 font-weight-bold text-center">713-864-5300 <br>lissette@greencaretx.com</p>
+			</div>
+		</div>
+	</div>
+</section>
 <section class="about-us-section">
 	<div class="content text-center">
 		<div class="row">
