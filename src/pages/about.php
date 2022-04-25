e93209e5308baedf10c16c9e44dce401f29b8362
Nico Watine <nico@studioncreations.com>
2022-04-25 11:22:40 -0500

    Structure prose wrap

--- src/pages/about.php
+++ src/pages/about.php
@@ -12,9 +12,11 @@ body-class: index
 	<div class="content text-center">
 		<div class="row">
 			<div class="column">
-				<h1 class="font-weight-bold">About Us</h1>
-				<p class="h4">Founded in 2002, Greencare Interior Plants, LLC, is a family-owned and operated business. We take pride in our reputation for reliable service, original design and great care of our customers and employees.</p>
-				<p class="h4">Working with both Commercial and Residential accounts throughout the Greater Houston area, we have the maintenance program and greenery selection to exceed the professional plant care service you expect.</p>
+				<div class="prose-wrap float-center">
+					<h1 class="font-weight-bold">About Us</h1>
+					<p class="h4">Founded in 2002, Greencare Interior Plants, LLC, is a family-owned and operated business. We take pride in our reputation for reliable service, original design and great care of our customers and employees.</p>
+					<p class="h4">Working with both Commercial and Residential accounts throughout the Greater Houston area, we have the maintenance program and greenery selection to exceed the professional plant care service you expect.</p>
+				</div>
 			</div>
 		</div>
 	</div>
