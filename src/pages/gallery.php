9b8af8bd73810b83185db8de5ce555c68ff9f9b1
Nico Watine <nico@studioncreations.com>
2022-04-26 11:06:31 -0500

    Copy markup from tacupra.com masonry gallery

--- src/pages/gallery.php
+++ src/pages/gallery.php
@@ -8,3 +8,63 @@ og-image:
 body-id: index
 body-class: index
 ---
+<section class="gallery-section">
+	<div id="masonryGrid" class="row small-up-2 medium-up-3">
+		<div class="tile column column-block">
+			<div class="tileBase">
+				<img src="/img/gallery/Greencare%20Interior%20Plants%20-%201.jpg" alt="">
+				<!-- <div class="padding-XY">
+					<p class="margin-0">First tacks</p>
+				</div> -->
+			</div>
+		</div>
+		<div class="tile column column-block">
+			<div class="tileBase">
+				<img src="/img/gallery/Greencare%20Interior%20Plants%20-%202.jpg" alt="">
+				<!-- <div class="padding-XY">
+					<p class="margin-0">A hole saw made a perfect sized hole for the transmission locating bushing</p>
+				</div> -->
+			</div>
+		</div>
+		<div class="tile column column-block">
+			<div class="tileBase">
+				<img src="/img/gallery/Greencare%20Interior%20Plants%20-%203.jpg" alt="">
+				<!-- <div class="padding-XY">
+					<p class="margin-0">A hole saw made a perfect sized hole for the transmission locating bushing</p>
+				</div> -->
+			</div>
+		</div>
+		<div class="tile column column-block">
+			<div class="tileBase">
+				<img src="/img/gallery/Greencare%20Interior%20Plants%20-%204.jpg" alt="">
+				<!-- <div class="padding-XY">
+					<p class="margin-0">A hole saw made a perfect sized hole for the transmission locating bushing</p>
+				</div> -->
+			</div>
+		</div>
+		<div class="tile column column-block">
+			<div class="tileBase">
+				<img src="/img/gallery/Greencare%20Interior%20Plants%20-%205.jpg" alt="">
+				<!-- <div class="padding-XY">
+					<p class="margin-0">A hole saw made a perfect sized hole for the transmission locating bushing</p>
+				</div> -->
+			</div>
+		</div>
+		<div class="tile column column-block">
+			<div class="tileBase">
+				<img src="/img/gallery/Greencare%20Interior%20Plants%20-%206.jpg" alt="">
+				<!-- <div class="padding-XY">
+					<p class="margin-0">A hole saw made a perfect sized hole for the transmission locating bushing</p>
+				</div> -->
+			</div>
+		</div>
+		<div class="tile column column-block">
+			<div class="tileBase">
+				<img src="/img/gallery/Greencare%20Interior%20Plants%20-%207.jpg" alt="">
+				<!-- <div class="padding-XY">
+					<p class="margin-0">A hole saw made a perfect sized hole for the transmission locating bushing</p>
+				</div> -->
+			</div>
+		</div>
+	</div>
+</section>
