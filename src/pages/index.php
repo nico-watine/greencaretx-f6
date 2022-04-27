fae91a35c8f4ca964f279c8c62db2f06078fcdfc
Nico Watine <nico@studioncreations.com>
2022-04-27 12:24:28 -0500

    Rename class and scss partial for accuracy

--- src/pages/index.php
+++ src/pages/index.php
@@ -34,7 +34,7 @@ body-class: index
 </div>
 
 <div id="main-content-section" data-magellan-target="main-content-section">
-	<section class="scheduling-banner light-banner">
+	<section class="contact-banner light-banner">
 		<div class="row">
 			<div class="large-7 columns prose-column">
 				<div class="prose-wrap float-center large-float-right">
@@ -116,7 +116,7 @@ body-class: index
 		</div>
 	</div>
 </section>
-<section class="scheduling-banner">
+<section class="contact-banner">
 	<div class="row">
 		<div class="large-7 columns prose-column">
 			<div class="prose-wrap float-center large-float-right">
@@ -125,7 +125,7 @@ body-class: index
 		</div>
 		<div class="large-5 columns contact-info-column">
 			<div class="prose-wrap float-center">
-				<p class="h3 font-weight-bold text-center">713-864-5300 <br>lissette@greencaretx.com</p>
+				<p class="h3 font-weight-bold text-center"><a href="tel:+1-713-864-5300">713-864-5300</a> <br><a href="mailto:lissette@greencaretx.com">lissette@greencaretx.com</a></p>
 			</div>
 		</div>
 	</div>
