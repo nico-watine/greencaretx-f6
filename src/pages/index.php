e2e04601ead5bbda4aa27ee7c722aaec2f64770b
Nico Watine <nico@studioncreations.com>
2022-04-27 12:54:55 -0500

    Insert first magellan quote link

--- src/pages/index.php
+++ src/pages/index.php
@@ -69,12 +69,12 @@ body-class: index
 		<h2 class="h2 section-heading font-weight-bold text-center mb-40 float-center">We offer 3 options to best suit your needs</h2>
 		<div class="grid-x grid-padding-x">
 			<div class="cell medium-4">
-				<div class="service-option-card flex-container flex-dir-column">
+				<div class="service-option-card flex-container flex-dir-column" data-magellan data-threshold="0">
 					<h3 class="h3 font-weight-bold">Purchase new plants</h3>
 					<div class="flex-child-auto position-relative">
 						<p class="absolute-center width-100">You purchase your new plants from us, along with their decorative containers, and we provide the weekly guaranteed maintenance.</p>
 					</div>
-					<a href="#" class="m-0 button large font-weight-bold mt-20 mb-0 border-rounded">Request Quote</a>
+					<a href="#contact-banner" class="m-0 button large font-weight-bold mt-20 mb-0 border-rounded">Request Quote</a>
 				</div>
 			</div>
 			<div class="cell medium-4">
@@ -116,7 +116,7 @@ body-class: index
 		</div>
 	</div>
 </section>
-<section class="contact-banner dark-banner">
+<section class="contact-banner dark-banner" id="contact-banner" data-magellan-target="contact-banner">
 	<div class="row">
 		<div class="large-7 columns prose-column">
 			<div class="prose-wrap float-center large-float-right">
