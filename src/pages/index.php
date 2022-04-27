6137f12e2de711b022eb8e35747a78e4f4e63f0c
Nico Watine <nico@studioncreations.com>
2022-04-27 12:56:17 -0500

    Link remaining “request quote” boxes

--- src/pages/index.php
+++ src/pages/index.php
@@ -83,7 +83,7 @@ body-class: index
 					<div class="flex-child-auto position-relative">
 						<p class="absolute-center width-100">We lease you the plants and their decorative containers, and we provide the weekly guaranteed maintenance.</p>
 					</div>
-					<a href="#" class="m-0 button large font-weight-bold mt-20 mb-0 border-rounded">Request Quote</a>
+					<a href="#contact-banner" class="m-0 button large font-weight-bold mt-20 mb-0 border-rounded">Request Quote</a>
 				</div>
 			</div>
 			<div class="cell medium-4">
@@ -92,7 +92,7 @@ body-class: index
 					<div class="flex-child-auto position-relative">
 						<p class="absolute-center width-100">If you happen to already have your own plants, we can take over their guaranteed maintenance.</p>
 					</div>
-					<a href="#" class="m-0 button large font-weight-bold mt-20 mb-0 border-rounded">Request Quote</a>
+					<a href="#contact-banner" class="m-0 button large font-weight-bold mt-20 mb-0 border-rounded">Request Quote</a>
 				</div>
 			</div>
 		</div>
