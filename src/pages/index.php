d0a05791f4c39c25734e2ab28d5a430f293b064c
Nico Watine <nico@studioncreations.com>
2022-04-27 11:21:43 -0500

    Add gallery section to index

--- src/pages/index.php
+++ src/pages/index.php
@@ -130,6 +130,45 @@ body-class: index
 		</div>
 	</div>
 </section>
+<section class="gallery-section">
+	<div id="masonryGrid" class="row small-up-2 medium-up-3">
+		<div class="tile column column-block">
+			<div class="tileBase">
+				<a href="/img/gallery/Greencare%20Interior%20Plants%20-%201.jpg"><img src="/img/gallery/Greencare%20Interior%20Plants%20-%201.jpg" alt=""></a>
+			</div>
+		</div>
+		<div class="tile column column-block">
+			<div class="tileBase">
+				<a href="/img/gallery/Greencare%20Interior%20Plants%20-%203.jpg"><img src="/img/gallery/Greencare%20Interior%20Plants%20-%203.jpg" alt=""></a>
+			</div>
+		</div>
+		<div class="tile column column-block">
+			<div class="tileBase">
+				<a href="/img/gallery/Greencare%20Interior%20Plants%20-%207.jpg"><img src="/img/gallery/Greencare%20Interior%20Plants%20-%207.jpg" alt=""></a>
+			</div>
+		</div>
+		<div class="tile column column-block">
+			<div class="tileBase">
+				<a href="/img/gallery/Greencare%20Interior%20Plants%20-%204.jpg"><img src="/img/gallery/Greencare%20Interior%20Plants%20-%204.jpg" alt=""></a>
+			</div>
+		</div>
+		<div class="tile column column-block">
+			<div class="tileBase">
+				<a href="/img/gallery/Greencare%20Interior%20Plants%20-%205.jpg"><img src="/img/gallery/Greencare%20Interior%20Plants%20-%205.jpg" alt=""></a>
+			</div>
+		</div>
+		<div class="tile column column-block">
+			<div class="tileBase">
+				<a href="/img/gallery/Greencare%20Interior%20Plants%20-%206.jpg"><img src="/img/gallery/Greencare%20Interior%20Plants%20-%206.jpg" alt=""></a>
+			</div>
+		</div>
+		<div class="tile column column-block">
+			<div class="tileBase">
+				<a href="/img/gallery/Greencare%20Interior%20Plants%20-%202.jpg"><img src="/img/gallery/Greencare%20Interior%20Plants%20-%202.jpg" alt=""></a>
+			</div>
+		</div>
+	</div>
+</section>
 <section class="about-us-section">
 	<div class="content text-center">
 		<div class="row">
