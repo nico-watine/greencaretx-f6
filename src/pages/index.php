560b70fdb72877e2fd697e5639f5e23c9406602f
Nico Watine <nico@studioncreations.com>
2022-04-28 11:54:42 -0500

    Tweak class name and markup

--- src/pages/index.php
+++ src/pages/index.php
@@ -9,12 +9,12 @@ body-id: index
 body-class: index
 ---
 <div class="hero-full-screen">
-	<div class="logo-wrap">
+	<header class="header-wrap">
 		<h1 class="text-hide">
 			<img class="logo" src="/img/logo/greencaretx-logo.svg" title="Greencare Interior Plants logo" alt="Greencare Interior Plants logo">
 			Greencare Interior Plants
 		</h1>
-	</div>
+	</header>
 	<div class="prose-wrap">
 		<h1 class="headline">Servicing Plants <br><span>Serving People</span></h1>
 		<h2 class="mission-statement float-center">As a family-owned business, providing <span class="display-inline-block">top-tier plant</span> service to ensure your complete satisfaction is our highest&nbsp;priority.</h2>
