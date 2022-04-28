834c0c9f5478fb033a166f2499ec55b9124e5c96
Nico Watine <nico@studioncreations.com>
2022-04-28 12:30:04 -0500

    Improve head yml

--- src/pages/index.php
+++ src/pages/index.php
@@ -1,9 +1,9 @@
 ---
 url: /
 title: Servicing Plants, Serving People
-description: Commercial &amp; Residential Indoor Plant Care Services - Call 713-864-5300 for your free consultation and next day quote!
-og-title: Servicing Plants, Serving People
-og-description: Commercial &amp; Residential Indoor Plant Care Services - Call 713-864-5300 for your free consultation and next day quote!
+description: Commercial &amp; Residential Indoor Plant Care Services in Houston, TX - Call for your free consultation!
+og-title: Servicing Plants, Serving People | Greencare Interior Plants
+og-description: Commercial &amp; Residential Indoor Plant Care Services in Houston, TX - Call 713-864-5300 for your free consultation and next day quote!
 og-image:
 body-id: index
 body-class: index
